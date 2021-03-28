d53a41d4253ee6c7aaa7691b39c38ce286caa90c
AdaZebra <rumusicmajor@yahoo.com>
2021-03-27 20:01:20 -0700

    Complete graph creation in python
    Still need to figure out how to connect the python to everything and plot those graphs in html?

--- alpha_vantage.py
+++ alpha_vantage.py
@@ -2,19 +2,32 @@ import requests
 import json
 from dotenv import dotenv_values
 import pandas as pd
+from bokeh.plotting import figure, show
 
-#load_dotenv()
-config = dotenv_values(".env")
+def make_graph(ticker_name, type_of_graph)
 
-key = config['VANTAGEAPI']
-ticker = 'AAPL'
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
-response = requests.get(url)
-data = [row.strip().split(',') for row in response.text.split('\n')]
+    config = dotenv_values(".env")
+    key = config['VANTAGEAPI']
+    ticker = ticker_name
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
+    response = requests.get(url)
 
-df_API = pd.DataFrame(data[1:-1], columns=data[0])
+    df_API = pd.DataFrame.from_dict(
+        response.json()['Time Series (Daily)'], 
+        orient='index')
 
-print(df_API.head())
+    df_API.columns=['open', 'high', 'low', 'close', 'adjusted close', 'volume', 'dividend amount', 'split coefficient']      
+
+    df_API.index = pd.to_datetime(df_API.index, format='%m/%d/%Y')
+    df_API[type_of_graph] = pd.to_numeric(df_API[type_of_graph])
+    df_API.info()
+
+    x = df_API.index.tolist()
+    y = df_API[type_of_graph].tolist()
+
+    p = figure(title="Stock Price at " + type_of_graph, x_axis_label='Month and Year', y_axis_label='Stock Price in USD', x_axis_type='datetime')
+    p.line(x, y, line_width=2)
+    show(p)
 
 
 
@@ -28,3 +41,5 @@ print(df_API.head())
 #https://github.com/theskumar/python-dotenv
 #https://www.alphavantage.co/documentation/#
 #https://medium.com/@patrick.collins_58673/stock-api-landscape-5c6e054ee631
+#https://programminghistorian.org/en/lessons/visualizing-with-bokeh
+#https://docs.bokeh.org/en/latest/docs/first_steps/first_steps_1.html
