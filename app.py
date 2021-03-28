6df4db36f1a2fa57bc1094677ddb17e4d1e6afca
AdaZebra <rumusicmajor@yahoo.com>
2021-03-28 00:26:52 -0700

    tab issue?

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
-   if request.method == 'POST':
+    if request.method == 'POST':
         result = request.form
         #plot = make_graph(ticker_name, type_of_graph)
         #script, div = components(plot)
@@ -54,4 +54,4 @@ def result():
         return render_template("result.html",result = result, the_div=div, the_script=script)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
